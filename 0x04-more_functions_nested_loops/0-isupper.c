d73c072e2c1f5c231aaabbe319e8692b4d0d805f
Ntiamoah1800 <nkwaku1800@gmail.com>
2022-10-20 11:54:03 -0700

    tasksupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * _isupper - checks if the character is uppercase
+ * @c: Variable text
+ * Return: ALways 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
