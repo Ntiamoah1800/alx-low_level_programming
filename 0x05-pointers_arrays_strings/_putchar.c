147d2e3d4ec3d12b1580329cc37512235ee95826
Ntiamoah1800 <nkwaku1800@gmail.com>
2022-10-24 23:32:33 -0700

    task8

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,9 +2,9 @@
 
 /**_putchar - writes character c to stdout
  * @c: char to be printed
- * Return: On success
+ * Return: On success 1
  */
 int _putchar(char c)
 {
-	return(writes(1, &c, 1));
+	return(write(1, &c, 1));
 }
