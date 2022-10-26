3b6c8e647fdfc3afcd2180937ddf4411c59f8aff
Ntiamoah1800 <nkwaku1800@gmail.com>
2022-10-25 17:58:30 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,18 +10,18 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int num = 0;
 
-	do {
-
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
+	{
 		if (*s == '-')
 			sign *= -1;
-
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
-			break;
-
-	} while (*s++);
-
+		if (*s == '+')
+			sign *= +1;
+		s++;
+	}
+	while ('0' <= *s && *s <= '9' && *s != '\0');
+	{
+		num = num * 10 + (*s - '0');
+		s++;
+	}
 	return (num * sign);
 }
