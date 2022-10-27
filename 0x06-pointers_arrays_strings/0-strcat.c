337032a5f451043a26f9fdf46be9c3294d3f010c
Ntiamoah1800 <nkwaku1800@gmail.com>
2022-10-27 15:23:42 -0700

    heyhey

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,17 +12,18 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0')
-		i++;
-
+	i = 0;
 	while (dest[i] != '\0')
 	{
-		*(dest + i) = src[j];
+		i++;
+	}
+	j = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[j];
 		j++;
 		i++;
 	}
-
-	*(dest + i) = '\0';
-
+	dest[i] = '\0';
 	return (dest);
 }
